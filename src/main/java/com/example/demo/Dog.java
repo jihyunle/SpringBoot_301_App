65d7d965826254a4a06c00ef92e8d0004e82fde1
jihyunle <jennifer.jlee@outlook.com>
2019-02-26 13:32:58 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/Dog.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity // creates a table in database
+public class Dog {
+    @Id // field is used for reach row in the database
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    private String name;
+
+    @Size(min = 1)
+    private String age;
+
+    public Dog(){
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
